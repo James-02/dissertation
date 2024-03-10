195e26448c359fc16ba714840a0e88659fce5101
James02 <james.newsome02@gmail.com>
2024-03-10 17:31:09 +0000

    update simulation model - generate reference signal to calculate phase lag against - initially compute period with coupling at 0 - load and normalise data into input phases

--- oscillator/run_sim.py
+++ oscillator/run_sim.py
@@ -1,14 +1,11 @@
 from dde import ddeint, dde_system
-from preprocessing import load_mnist
-from utils import compute_phase
+from preprocessing import load_mnist, load_ecg, normalise_to_phases
+from utils import compute_phase, prof_pulse, compute_period
 from visualisation import plot_danino
 
 import numpy as np
 
 def initial_conditions(t):
-    """
-    Define the initial conditions for the dde system
-    """
     return [0, 100, 0, 0]
 
 def run_danino():
@@ -16,29 +13,47 @@ def run_danino():
     params = {
         'CA': 1, 'CI': 4, 'del_': 1e-3, 'alpha': 2500, 'k': 1, 'k1': 0.1, 'b': 0.06, 'gammaA': 15,
         'gammaI': 24, 'gammaH': 0.01, 'f': 0.3, 'g': 0.01, 'd': 0.7, 'd0': 0.88, 'D': 2.5, 'mu': 0.6,
-        'period': 1.5, 'phase': 0.1, 'coupling': 0.1, 'delay': 10, 'n': 10
+        'delay': 10, 'n': 10, 'period': 0, 'phase': 0, 'coupling': 0
     }
 
-    # Load training and testing data
-    X_train, Y_train, X_test, Y_test = load_mnist()
+    # Load train and test datasets
+    X_train, Y_train, X_test, Y_test = load_ecg()
 
-    # Time points [0 - 1000], with 1000 intervals (+1 increments)
-    time = np.linspace(0, 1000, 1000)
+    # Time points [0 - 1000], with intervals of the training set size
+    intervals = 187
+    time = np.linspace(0, 1000, intervals)
 
-    # TODO: Use data as input to the oscillator
-        # TODO: Create input phase and set and set as params to the simulator
+    # Normalise data to 0-180 degree phases and use as input phase
+    X_train_normalised = normalise_to_phases(X_train)
+    input_phase = X_train_normalised[0][0]
+    print("Input Phase: " + str(input_phase))
+
+    # initial run with no coupling to compute the period
+    results = ddeint(dde_system, initial_conditions, time, params['delay'], args=(params,))
+
+    # Extract the A signal from initial run for use to calculate the period
+    A_signal = [row[0] for row in results]
+
+    # Calculate the period
+    period = compute_period(A_signal, np.mean(np.diff(time)))
+    print("Calculated Period: " + str(period))
+
+    # Update the parameters to add coupling, the calculated period and input phase
+    params.update({'period': period, 'input_phase': input_phase, 'coupling': 0.007})
+
+    # Generate a reference signal at phase 0
+    reference_signal = prof_pulse(time, params['period'], 0)
 
     # Solve the delayed differential equations to get the derivatives of the system variables
     results = ddeint(dde_system, initial_conditions, time, params['delay'], args=(params,))
 
-    # Compute the phases of the waveform
-    # TODO: Is this correct to calculate the phases?
-    phases = np.array([compute_phase(y) for y in results])
+    # Extract the A signal
+    A_signal = [row[0] for row in results]
 
-    # TODO: Use phases to feed into next node for coupled oscillator model
-
-    # Plot the phases over time
-    plot_danino(time, phases)
+    # Calculate the phase between the reference signal and the A signal
+    phase = compute_phase(A_signal, reference_signal, period, np.mean(np.diff(time)))
+    print("Output Phase: " + str(phase))
+    # This phase is now the output of the node (the state)
 
     # Plot the derivatives over time
     plot_danino(time, results)
