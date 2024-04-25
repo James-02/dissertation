22541ebc9612d683b0241a9e3de2b08c75f95bb5
James02 <james.newsome02@gmail.com>
2024-04-25 20:17:13 +0100

    classification update: - add noise in cross validation to training sets - save train/test states + labels for readout analysis

--- utils/classification.py
+++ utils/classification.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, r2_score, classification_report
 
 from utils.logger import Logger
-from utils.preprocessing import save_npz
+from utils.preprocessing import save_npz, augment_data
 from utils.analysis import log_params, log_metrics, compute_mean_metrics
 
 logger = Logger()
@@ -46,12 +46,14 @@ def run_fold(reservoir, readout, X_train_fold, Y_train_fold, X_val, Y_val, fold_
     fold_metrics = evaluate_performance(Y_val, Y_pred, runtime)
 
     if save_file:
-        data = {"model-hypers": {**reservoir.hypers, **readout.hypers}, "test_states": test_states, "Y_pred": Y_pred, "metrics": fold_metrics}
+        data = {"model-hypers": {**reservoir.hypers, **readout.hypers}, "train_states": train_states, "test_states": test_states, 
+                "Y_train": Y_train_fold, "Y_test": Y_val, "Y_pred": Y_pred, "metrics": fold_metrics}
         save_npz(filename=f"{save_file}-fold-{fold_index}.npz", **data)
 
     return fold_metrics
 
-def cross_validate(reservoir: Node, readout: Node, X: np.ndarray, Y: np.ndarray, folds: int = 5, save_file: str = None):
+def cross_validate(reservoir: Node, readout: Node, X: np.ndarray, Y: np.ndarray, folds: int = 5, 
+                   save_file: str = None, noise_rate: float = 0, noise_ratio: float = 0):
     kf = KFold(n_splits=folds, shuffle=True)
     fold_results = []
     fold_metrics = []
@@ -64,6 +66,10 @@ def cross_validate(reservoir: Node, readout: Node, X: np.ndarray, Y: np.ndarray,
         for i, (train_index, val_index) in enumerate(kf.split(X)):
             X_train_fold, X_val = X[train_index], X[val_index]
             Y_train_fold, Y_val = Y[train_index], Y[val_index]
+
+            # add noise augmentation to training set
+            X_train_fold, Y_train_fold = augment_data(X_train_fold, Y_train_fold, noise_rate, noise_ratio)
+
             fold_result = pool.apply_async(run_fold_partial, args=(X_train_fold, Y_train_fold, X_val, Y_val, i, save_file))
             fold_results.append(fold_result)
 
@@ -73,8 +79,8 @@ def cross_validate(reservoir: Node, readout: Node, X: np.ndarray, Y: np.ndarray,
     return compute_mean_metrics(fold_metrics)
 
 def classify(reservoir: Node, readout: Node, X_train: np.ndarray, Y_train: np.ndarray, X_test: np.ndarray, 
-             Y_test: np.ndarray, folds: int = None, save_file: str = None):
-    
+             Y_test: np.ndarray, folds: int = None, save_file: str = None, noise_rate: float = 0, noise_ratio: float = 0):
+
     # log model hyper-parameters
     log_params({**reservoir.hypers, **readout.hypers}, title="Reservoir Hyper-parameters")
 
@@ -82,7 +88,7 @@ def classify(reservoir: Node, readout: Node, X_train: np.ndarray, Y_train: np.nd
     if folds:
         X = np.concatenate((X_train, X_test), axis=0)
         Y = np.concatenate((Y_train, Y_test), axis=0)
-        metrics = cross_validate(reservoir, readout, X, Y, folds=folds, save_file=save_file)
+        metrics = cross_validate(reservoir, readout, X, Y, folds=folds, save_file=save_file, noise_rate=noise_rate, noise_ratio=noise_ratio)
 
     # perform train / test classification
     else:
@@ -101,13 +107,14 @@ def classify(reservoir: Node, readout: Node, X_train: np.ndarray, Y_train: np.nd
 
         # save performance metrics to file
         if save_file:
-            data = {"model-hypers": {**reservoir.hypers, **readout.hypers}, "test_states": test_states, "Y_pred": Y_pred, "metrics": metrics}
-            save_npz(filename=save_file, **data)
+            data = {"model-hypers": {**reservoir.hypers, **readout.hypers}, "train_states": train_states, "Y_train": Y_train, 
+                    "test_states": test_states, "Y_test": Y_test, "Y_pred": Y_pred, "metrics": metrics}
+            save_npz(filename=save_file + ".npz", **data)
 
     log_metrics(metrics)
     return metrics
 
-def evaluate_performance(Y_true: np.ndarray, Y_pred: np.ndarray, time: float):
+def evaluate_performance(Y_true: np.ndarray, Y_pred: np.ndarray, time: float = 0):
     logger.info("Calculating model performance metrics")
     Y_true = np.array([np.argmax(y_t) for y_t in Y_true])
     Y_pred = np.array([np.argmax(y_p) for y_p in Y_pred])
